c99d09cf32f60a9ff38ec91c9da7fbd17d076023
Emerson Detering <detering@bu.edu>
2022-04-17 16:05:30 -0400

    Added a Startgame/stopgame button, Added Deal button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->pushButton_2->setText("Stop Game");
+}
+
+
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label->setPixmap(QPixmap(":/CardImages/svg-cards/9_of_clubs.svg") );
+}
+
